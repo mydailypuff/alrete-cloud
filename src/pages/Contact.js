93a6ba7643bb8632185e6d11fe6efe0284d6857e
SANJHK-BLRM24 <sanjanahk11@gmail.com>
2024-09-24 18:18:07 +0530

    update mail

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 import Notiflix from 'notiflix';
 
 const Contact = () => {
-    useDocTitle('MLD | Molad e Konsult - Send us a message')
+    useDocTitle('Alrete | Alrete Cloud - Send us a message')
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
     const [email, setEmail] = useState('')
